f8e3c84f775cea49e07fbcb62e7c91ae03eec2a7
saljuama <saljuama@gmail.com>
2020-11-28 19:43:20 +0100

    Refactor high card rule

--- src/main/scala/poker/HandComparator.scala
+++ src/main/scala/poker/HandComparator.scala
@@ -2,32 +2,34 @@ package poker
 
 class HandComparator {
   def compare(firstPlayerHand: String, secondPlayerHand: String): Int = {
-    def valuesFromHand(hand: String): Seq[Int] = hand.split(" ").toSeq
-      .map(card => cardValueToInt(card.substring(0, 1)))
-      .sorted(Ordering.Int.reverse)
 
-    val first = valuesFromHand(firstPlayerHand)
-    val second = valuesFromHand(secondPlayerHand)
-
-    Range.inclusive(0, 4)
-      .map(index => {
-        val f = first(index)
-        val s = second(index)
-        if (f > s) 1
-        else if (f < s) 2
-        else 0
-      })
-      .find(x => x != 0)
-      .getOrElse(0)
-
+    highestCardRule(
+      valuesFromHand(firstPlayerHand),
+      valuesFromHand(secondPlayerHand)
+    )
   }
 
-  private def cardValueToInt(number: String) = {
-    if (number == "A") 14
-    else if (number == "K") 13
-    else if (number == "Q") 12
-    else if (number == "J") 11
-    else if (number == "T") 10
-    else Integer.parseInt(number)
+  private def highestCardRule(first: Seq[Int], second: Seq[Int]): Int = {
+    Range.inclusive(0, 4)
+      .map { index => (first(index), second(index)) }
+      .map {
+        case (f, s) if f > s => 1
+        case (f, s) if f < s => 2
+        case _ => 0
+      }
+      .find(x => x != 0)
+      .getOrElse(0)
+  }
+
+  private def valuesFromHand(hand: String): Seq[Int] = {
+    hand
+      .split(" ")
+      .map(card => cardValueToInt(card.substring(0, 1)))
+      .sorted(Ordering.Int.reverse)
+  }
+
+  private def cardValueToInt(number: String): Int = {
+    Map("A" -> 14, "K" -> 13, "Q" -> 12, "J" -> 11, "T" -> 10)
+      .getOrElse(number, Integer.parseInt(number))
   }
 }
